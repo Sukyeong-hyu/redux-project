8f0d71c1471ba5e423b6ed345af8f9424a5c1c49
Sukyeong-hyu <zztnrudzz13@naver.com>
2022-01-19 17:07:09 +0900

    Update Counter using Vanilla Javascript and Redux

--- reduxpractice/src/index.js
+++ reduxpractice/src/index.js
@@ -1,23 +1,44 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
+number.innerText = 0;
 
-number.innerText = count;
+const ADD = 'ADD'; // make string into const variable to avoid mistakes!
+const MINUS = 'MINUS';
 
-const updateText = () => {
-  number.innerText = count;
+// reducer: function that 'modifies' your data (return the data)
+// action: use when to call the reducer, when to modify the data (action should be object!)
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
+// store : place to put your data(state)
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+// subscribe: allow us to listen to the changes in the store
+countStore.subscribe(onChange);
+
+// dispatch: function that triggers reducer and put the parameter into the action
 const handleAdd = () => {
-  count++;
-  updateText();
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  count--;
-  updateText();
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener('click', handleAdd);
